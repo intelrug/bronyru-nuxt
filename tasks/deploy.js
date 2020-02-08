1fc328f4d899f318ed7c2ed9999b0d47be3073d1
Svyatoslav Fyodorov <intelrug@gmail.com>
2020-02-08 20:53:26 +0400

    Update deploy script

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,20 +1,16 @@
 const ghPages = require('gh-pages');
-const path = require('path');
-const fs = require('fs-extra');
 
-try {
-  fs.removeSync(path.resolve(__dirname, '../dist/'));
-  fs.copySync(path.resolve(__dirname, '../static/'), path.resolve(__dirname, '../dist/static/'));
-  fs.copySync(path.resolve(__dirname, '../.nuxt/'), path.resolve(__dirname, '../dist/.nuxt/'));
-  fs.copySync(path.resolve(__dirname, '../package.json'), path.resolve(__dirname, '../dist/package.json'));
-  fs.copySync(path.resolve(__dirname, '../tsconfig.json'), path.resolve(__dirname, '../dist/tsconfig.json'));
-  fs.copySync(path.resolve(__dirname, '../yarn.lock'), path.resolve(__dirname, '../dist/yarn.lock'));
-
-  ghPages.publish('dist', {
-    branch: 'dist',
-  }, () => {
-    fs.removeSync(path.resolve(__dirname, '../dist/'));
-  });
-} catch (err) {
-  console.error(err);
-}
+ghPages.publish('./', {
+  src: [
+    '.nuxt/**/*',
+    'static/**/*',
+    'package.json',
+    'tsconfig.json',
+    'yarn.lock',
+  ],
+  branch: 'dist',
+}, (e) => {
+  if (e) {
+    console.error(e);
+  }
+});
